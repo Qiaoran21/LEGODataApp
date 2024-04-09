8854ffb41e7607edc3ab059bca19fa1ea6a44bff
Qiaoran21 <qiaoran@hotmail.com>
2024-04-09 17:28:09 -0500

    QR code scanner added

--- app/src/main/java/com/example/legodataapp/screens/Home.kt
+++ app/src/main/java/com/example/legodataapp/screens/Home.kt
@@ -1,7 +1,13 @@
 package com.example.legodataapp.screens
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -12,6 +18,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
@@ -31,11 +38,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.example.legodataapp.NavItem
@@ -47,11 +57,66 @@ import com.example.legodataapp.ui.theme.fontFamily
 import com.example.legodataapp.data.LegoSet
 import com.example.legodataapp.model.SetViewModel
 import com.example.legodataapp.ui.theme.LightBrown
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun HomeScreen(setViewModel: SetViewModel, navController: NavController) {
     val sets by setViewModel.sets.observeAsState()
     var searchInput by remember { mutableStateOf("") }
+    var textResult by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    val qrCodeLauncher = rememberLauncherForActivityResult(ScanContract()) {
+        result ->
+        if (result.contents == null) {
+            textResult = "ERROR"
+        } else {
+            textResult = result.contents
+            if (textResult.startsWith("http")) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(result.contents))
+                context.startActivity(intent)
+            } else {
+                navController.navigate("${NavItem.QrCode.route}/${result.contents}")
+            }
+        }
+    }
+
+    fun showCamera() {
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan QR Code")
+        options.setCameraId(0)
+        options.setBeepEnabled(false)
+        options.setOrientationLocked(false)
+
+        qrCodeLauncher.launch(options)
+    }
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        isGranted ->
+        if (isGranted) {
+            showCamera()
+        } else {
+            textResult = "Camera permission denied."
+        }
+    }
+
+    fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+            ){
+            showCamera()
+        }
+        else {
+            requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+        }
+    }
 
     LaunchedEffect(Unit) {
         setViewModel.fetchSets()
@@ -67,6 +132,15 @@ fun HomeScreen(setViewModel: SetViewModel, navController: NavController) {
             searchInput = searchInput,
             onSearchInputChanged = { searchInput = it }
         )
+
+        FloatingActionButton(
+            onClick = { checkCameraPermission() },
+            backgroundColor = DarkYellow) {
+            Icon(
+                painter = painterResource(id = R.drawable.qr_scan),
+                contentDescription = "QR scan"
+            )
+        }
 
         LazyColumn(
             modifier = Modifier
